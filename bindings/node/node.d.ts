5b202ed0e452f88ddcaf20a350c6ec71e30b8ad0
tekrekon <lukaSiernos@gmail.com>
2026-07-18 23:57:54 -0400

    Format verifier SDK exports

--- bindings/node/node.d.ts
+++ bindings/node/node.d.ts
@@ -1,5 +1,1 @@
-export {
-	Verifier,
-	verify_bundle,
-	verify_bundle_at
-} from '../../pkg/browser/stogas_verifier.js';
+export { Verifier, verify_bundle, verify_bundle_at } from '../../pkg/browser/stogas_verifier.js';
--- bindings/node/node.js
+++ bindings/node/node.js
@@ -9,8 +9,4 @@ import {
 const wasm = readFileSync(new URL('../../pkg/browser/stogas_verifier_bg.wasm', import.meta.url));
 initSync({ module: wasm });
 
-export {
-	Verifier,
-	verify_bundle,
-	verify_bundle_at
-};
+export { Verifier, verify_bundle, verify_bundle_at };
